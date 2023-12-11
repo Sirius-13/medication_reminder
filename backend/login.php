d733f68a5992e66b197b64fc62ef0017b32b315f
Sirius-13 <cwxdesmond2002@gmail.com>
2023-12-12 00:33:19 +0800

    1. Convert login.php prepared statement  2. Tuning UI

--- backend/login.php
+++ backend/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$servername = "localhost"; 
+$servername = "localhost";
 $username = "root";
 $password = "";
 $database = "medication_reminder";
@@ -17,22 +17,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        $sql = "SELECT Username, PasswordHash FROM users WHERE Email = '$email'";
-        $result = $conn->query($sql);
+        $sql = "SELECT Username, PasswordHash FROM users WHERE Email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
         if ($result->num_rows > 0) {
             $row = $result->fetch_assoc();
             $hashedPassword = $row['PasswordHash'];
-        
+
             if (password_verify($password, $hashedPassword)) {
                 $token = bin2hex(random_bytes(32));
                 $_SESSION['token'] = $token;
                 $_SESSION['email'] = $email;
                 $username = $row['Username'];
-        
-                $updateTimestampSQL = "UPDATE users SET LastLoginDate = CURRENT_TIMESTAMP WHERE Email = '$email'";
-                $conn->query($updateTimestampSQL);
-        
+
+                $updateTimestampSQL = "UPDATE users SET LastLoginDate = CURRENT_TIMESTAMP WHERE Email = ?";
+                $stmt = $conn->prepare($updateTimestampSQL);
+                $stmt->bind_param("s", $email);
+                $stmt->execute();
+
                 echo "<script>
                     sessionStorage.setItem('token', '$token');
                     sessionStorage.setItem('email', '$email');
@@ -50,7 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 alert('Invalid username or password. Please try again.');
                 window.location.href = '../frontend/index.html';
             </script>";
-        }        
+        }
     }
 }
 
--- javascript/getCurrentTime.js
+++ javascript/getCurrentTime.js
@@ -5,15 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
         var date = new Date();
         var utcString = date.toUTCString();
 
-        // Splitting the UTC string to remove 'GMT'
         var parts = utcString.split(' ');
         var filteredParts = parts.filter(part => part !== 'GMT');
 
-        // Reconstructing the string without 'GMT'
         var filteredString = filteredParts.join(' ');
 
         clockElement.textContent = filteredString;
     }
-
     setInterval(clock, 1000);
 });
